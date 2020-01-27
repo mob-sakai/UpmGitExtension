1c8c2a0f8fb001b07935d12d7fac9f20a9cb6cc8
mob-sakai <sakai@mobcast.jp>
2020-01-27 10:05:46 +0900

    feat: add develop menu

---
+++ Packages/com.coffee.upm-git-extension/Editor/Coffee.UpmGitExtension.Dev/UpmGitExtensionMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using UnityEditor;
+
+namespace Coffee.UpmGitExtension.Dev
+{
+    class UpmGitExtensionMenu
+    {
+        const string kDevelopModeText = "UpmGitExtension/Develop Mode";
+        const string kDevelopModeSymbol = "UGE_DEV";
+
+        const string kEnableLoggingText = "UpmGitExtension/Enable Logging";
+        const string kEnableLoggingSymbol = "UGE_LOG";
+
+        [MenuItem(kDevelopModeText, false)]
+        static void DevelopMode()
+        {
+            SwitchSymbol(kDevelopModeSymbol);
+        }
+
+        [MenuItem(kDevelopModeText, true)]
+        static bool DevelopMode_Valid()
+        {
+            Menu.SetChecked(kDevelopModeText, HasSymbol(kDevelopModeSymbol));
+            return true;
+        }
+
+        [MenuItem(kEnableLoggingText, false)]
+        static void EnableLogging()
+        {
+            SwitchSymbol(kEnableLoggingSymbol);
+        }
+
+        [MenuItem(kEnableLoggingText, true)]
+        static bool EnableLogging_Valid()
+        {
+            Menu.SetChecked(kEnableLoggingText, HasSymbol(kEnableLoggingSymbol));
+            return true;
+        }
+
+        static string[] GetSymbols()
+        {
+            return PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup).Split(';', ',');
+        }
+
+        static void SetSymbols(string[] symbols)
+        {
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, string.Join(";", symbols));
+        }
+
+        static bool HasSymbol(string symbol)
+        {
+            return GetSymbols().Any(x => x == symbol);
+        }
+
+        static void SwitchSymbol(string symbol)
+        {
+            var symbols = GetSymbols();
+            SetSymbols(symbols.Any(x => x == symbol)
+                ? symbols.Where(x => x != symbol).ToArray()
+                : symbols.Concat(new[] { symbol }).ToArray()
+            );
+        }
+    }
+}
