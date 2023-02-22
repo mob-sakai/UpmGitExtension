7aa7e832ecd66f55f6f38519f05baa5ccc2ab429
mob-sakai <sakai861104@gmail.com>
2023-02-22 17:22:00 +0900

    fix test for 2023.1

--- Assets/Tests/ReflectionTests.cs
+++ Assets/Tests/ReflectionTests.cs
@@ -1,6 +1,4 @@
 #if UNITY_2020_1_OR_NEWER
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using UnityEngine;
@@ -15,14 +13,16 @@ namespace Coffee.UpmGitExtension
 {
     public class ReflectionTests
     {
+#if !UNITY_2023_1_OR_NEWER
         [Test]
         public void PackageDatabase_OnPackagesChanged()
         {
             Assert.True(
-                GitPackageDatabase._packageDatabase.Has("OnPackagesChanged", new object[] { Enumerable.Empty<IPackage>() }),
+                GitPackageDatabase._packageDatabase.Has("OnPackagesChanged", Enumerable.Empty<IPackage>()),
                 "void PackageDatabase.OnPackageChanged(IEnumerable<IPackage>) is not found"
             );
         }
+#endif
 
         [Test]
         public void UpmPackageVersion_Tag()
@@ -32,12 +32,22 @@ namespace Coffee.UpmGitExtension
                 "PackageTag UpmPackageVersion.m_Tag is not found"
             );
         }
-#if UNITY_2021_2_OR_NEWER
+
+#if UNITY_2023_1_OR_NEWER
+        [Test]
+        public void PackageDetailsLinks_AddToParentWithSeparator()
+        {
+            Assert.True(
+                new PackageDetailsLinks().Has("AddToParentWithSeparator", new VisualElement(), new Button()),
+                "void PackageDetailsLinks.AddToParentWithSeparator is not found"
+            );
+        }
+#elif UNITY_2021_2_OR_NEWER
         [Test]
         public void PackageDetailsLinks_AddToLinks()
         {
             Assert.True(
-                new PackageDetailsLinks().Has("AddToLinks", new object[] { new VisualElement(), new Button(), true }),
+                new PackageDetailsLinks().Has("AddToLinks", new VisualElement(), new Button(), true),
                 "void PackageDetailsLinks.AddToLink is not found"
             );
         }
@@ -46,7 +56,7 @@ namespace Coffee.UpmGitExtension
         public void PackageDetails_AddToLinks()
         {
             Assert.True(
-                new PackageDetails().Has("AddToLinks", new object[] { new Button() }),
+                new PackageDetails().Has("AddToLinks", new Button()),
                 "void PackageDetails.AddToLink is not found"
             );
         }
@@ -56,7 +66,7 @@ namespace Coffee.UpmGitExtension
         public void Clickable_Invoke()
         {
             Assert.True(
-                new Clickable(() => { }).Has("Invoke", new object[] { new MouseDownEvent() }),
+                new Clickable(() => { }).Has("Invoke", new MouseDownEvent()),
                 "void Clickable.Invoke(BaseEvent) is not found"
             );
         }
