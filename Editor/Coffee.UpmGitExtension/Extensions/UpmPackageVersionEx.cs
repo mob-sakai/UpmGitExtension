c32998e2abe94e39c57c761e39c98d69088d3e3d
mob-sakai <sakai861104@gmail.com>
2023-03-16 14:51:49 +0900

    fix: support future Unity versions

--- Editor/Coffee.UpmGitExtension/Extensions/UpmPackageVersionEx.cs
+++ Editor/Coffee.UpmGitExtension/Extensions/UpmPackageVersionEx.cs
@@ -7,11 +7,24 @@
 #elif UNITY_2020_3 || UNITY_2021_3 || UNITY_2022_2_OR_NEWER
 #define PACKAGE_INFO_HAS_BEEN_REMOVED
 #endif
+
+// V1: -2020.1.4f1
+// V2: 2020.1.5f1-, 2020.2.0f1-, 2020.3.0f1-, 2021.1.0f1-, 2021.2.0f1-, 2021.3.0f1-2021.3.20f1, 2022.1.0f1-, 2022.2.0f1-2022.2.9f1
+// V3: 2021.3.21f1-, 2022.2.10f1-, 2023.1.0f1-, 2023.2.0f1-
 #if UNITY_2020_1_0 || UNITY_2020_1_1 || UNITY_2020_1_2 || UNITY_2020_1_3 || UNITY_2020_1_4
 #define CONSTRACTOR_V1
-#endif
-#if UNITY_2021_3_21 || UNITY_2022_2_10 || UNITY_2023_1_0 || UNITY_2023_2_0
-#define REGISTRY_TYPE_HAS_BEEN_ADDED
+#elif UNITY_2020_1 || UNITY_2020_2 || UNITY_2020_3 || UNITY_2021_1 || UNITY_2021_2
+#define CONSTRACTOR_V2
+#elif UNITY_2021_3_0 || UNITY_2021_3_1 || UNITY_2021_3_2 || UNITY_2021_3_3 || UNITY_2021_3_4 || UNITY_2021_3_5 || UNITY_2021_3_6 || UNITY_2021_3_7 || UNITY_2021_3_8  || UNITY_2021_3_9
+#define CONSTRACTOR_V2
+#elif UNITY_2021_3_10 || UNITY_2021_3_11 || UNITY_2021_3_12 || UNITY_2021_3_13 || UNITY_2021_3_14 || UNITY_2021_3_15 || UNITY_2021_3_16 || UNITY_2021_3_17 || UNITY_2021_3_18  || UNITY_2021_3_19 || UNITY_2021_3_20
+#define CONSTRACTOR_V2
+#elif UNITY_2022_1
+#define CONSTRACTOR_V2
+#elif UNITY_2022_2_0 || UNITY_2022_2_1 || UNITY_2022_2_2 || UNITY_2022_2_3 || UNITY_2022_2_4 || UNITY_2022_2_5 || UNITY_2022_2_6 || UNITY_2022_2_7 || UNITY_2022_2_8  || UNITY_2022_2_9
+#define CONSTRACTOR_V2
+#elif UNITY_2021_3 || UNITY_2022_2 || UNITY_2023_1_OR_NEWER || UNITY_2023_2_OR_NEWER
+#define CONSTRACTOR_V3
 #endif
 
 using System;
@@ -38,42 +51,12 @@ namespace Coffee.UpmGitExtension
         public UnityEditor.PackageManager.PackageInfo packageInfo => m_PackageInfo;
 #endif
 
-#if !REGISTRY_TYPE_HAS_BEEN_ADDED
-        public UpmPackageVersionEx(UnityEditor.PackageManager.PackageInfo packageInfo, bool isInstalled, bool isUnityPackage)
-#else
-        public UpmPackageVersionEx(UnityEditor.PackageManager.PackageInfo packageInfo, bool isInstalled, RegistryType availableRegistry)
-#endif
-#if CONSTRACTOR_V1
-            : base(packageInfo, isInstalled)
-#elif !REGISTRY_TYPE_HAS_BEEN_ADDED
-            : base(packageInfo, isInstalled, isUnityPackage)
-#else
-            : base(packageInfo, isInstalled, availableRegistry)
-#endif
-        {
-        }
-
-#if !REGISTRY_TYPE_HAS_BEEN_ADDED
-        public UpmPackageVersionEx(UnityEditor.PackageManager.PackageInfo packageInfo, bool isInstalled, SemVersion? version, string displayName, bool isUnityPackage)
-#else
-        public UpmPackageVersionEx(UnityEditor.PackageManager.PackageInfo packageInfo, bool isInstalled, SemVersion? version, string displayName, RegistryType availableRegistry)
-#endif
-#if CONSTRACTOR_V1
-            : base(packageInfo, isInstalled, version, displayName)
-#elif !REGISTRY_TYPE_HAS_BEEN_ADDED
-            : base(packageInfo, isInstalled, version, displayName, isUnityPackage)
-#else
-            : base(packageInfo, isInstalled, version, displayName, availableRegistry)
-#endif
-        {
-        }
-
         public UpmPackageVersionEx(UpmPackageVersion packageVersion)
 #if CONSTRACTOR_V1
             : base(packageVersion.GetPackageInfo(), packageVersion.isInstalled)
-#elif !REGISTRY_TYPE_HAS_BEEN_ADDED
+#elif CONSTRACTOR_V2
             : base(packageVersion.GetPackageInfo(), packageVersion.isInstalled, packageVersion.isUnityPackage)
-#else
+#elif CONSTRACTOR_V3
             : base(packageVersion.GetPackageInfo(), packageVersion.isInstalled, packageVersion.availableRegistry)
 #endif
         {
